d22a3e2e5c0cb4eead6072d2221e9f9749bdcc0c
rajkbhakta <rajkbhakta@gmail.com>
2022-11-21 20:34:46 -0600

    4th popup box, added icons, changed heading

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,19 +1,14 @@
-// HTML element declarations
-const enterEl = document.getElementById('enter-button');
-const apiFieldEl = document.getElementById('api-key');
-const goldFieldEl = document.getElementById('gold');
-const gemsFieldEl = document.getElementById('gems');
-const usdFieldEl = document.getElementById('usd');
-const currencyFieldEl = document.getElementById('my-currency');
+const enterEl = document.getElementById("enter-button");
+const apiFieldEl = document.getElementById("api-key");
+const goldFieldEl = document.getElementById("gold");
+const gemsFieldEl = document.getElementById("gems");
+const usdFieldEl = document.getElementById("usd");
+const currencyFieldEl = document.getElementById("my-currency");
 
 // Testing API Keys: 3866BD83-5D2B-AA46-8859-518486210B510E1ED7BA-9AE9-49C2-8035-A5B53A93DF06 | 6A8C3A68-7264-054E-8E91-6E368B2C223B803FA554-3434-402A-B047-C8657E85F416
-
-// ------------------- GW2 API JS --------------------
-
-// GLOBALS----------------------
-let gwApiKey = '';
-let usdValue = '';
-let newUsdValue = '';
+let gwApiKey = "";
+let usdValue = "";
+let newUsdValue = "";
 
 // FUNCTIONS--------------------
 
@@ -28,9 +23,7 @@ function getApi(key) {
     })
     .then(function (data) {
       //console.log(data[0].value);
-      // Pulls the value of the entered key account's gold value
-      goldFieldEl.textContent = data[0].value
-      // Looks up entered GW2's global gold to gem conversion rate, based on amount of gold from previous input
+      goldFieldEl.textContent = data[0].value;
       let requestOtherUrl = `https://api.guildwars2.com/v2/commerce/exchange/coins?quantity=${data[0].value}`;
 
       fetch(requestOtherUrl)
@@ -39,16 +32,12 @@ function getApi(key) {
         })
         .then(function (data1) {
           //console.log(data1.quantity);
-          // Pulls the amount of gems the entered key account's gold converts into
-          gemsFieldEl.textContent = data1.quantity
-          // Converts account's gem value into USD based on a non-fluctuating amount at a rate of 1 Gem = .0125 Cents
+          gemsFieldEl.textContent = data1.quantity;
           usdValue = Math.round(data1.quantity * 0.0125 * 100) / 100;
           //console.log(`$${parseFloat(usdValue).toFixed(2)}`);
-          // Adjusts USD value to present it in a ledgeable format, aka 2 decimal cents and all dollars
-          newUsdValue = parseFloat(usdValue).toFixed(2)
-          // Further adjusts displayed USD to include "$" sign, then displays it
-          let displayUsdValue = `$${newUsdValue}`
-          usdFieldEl.textContent = displayUsdValue
+          newUsdValue = parseFloat(usdValue).toFixed(2);
+          let displayUsdValue = `$${newUsdValue}`;
+          usdFieldEl.textContent = displayUsdValue;
           currencyExchange();
         });
     });
@@ -64,9 +53,23 @@ var currencyRateArray = [];
 // This webpage will allow users to select the following 15
 // currency options as a base option.
 // It is possible to push more to codes to this array if desired
-var acceptedCurrencyCodeArray = ["MXN", "EUR", "JPY", "GBP", "CNY", 
-                               "AUD", "CAD", "CHF", "HKD", "SGD",
-                               "SEK", "KRW", "NOK", "NZD", "INR"];
+var acceptedCurrencyCodeArray = [
+  "MXN",
+  "EUR",
+  "JPY",
+  "GBP",
+  "CNY",
+  "AUD",
+  "CAD",
+  "CHF",
+  "HKD",
+  "SGD",
+  "SEK",
+  "KRW",
+  "NOK",
+  "NZD",
+  "INR",
+];
 var acceptedCurrencyCodeString = acceptedCurrencyCodeArray.toString();
 // console.log(acceptedCurrencyCodeString);
 // This array will hold the rates corresponding to the
@@ -85,56 +88,52 @@ var exchangeUrl = `https://api.currencyfreaks.com/latest?apikey=${exchangeApiKey
 // FUNCTIONS--------------------
 
 function init() {
-    // retrieve latest data from exchange rate API
-    getExchangeRate();
-
+  // retrieve latest data from exchange rate API
+  getExchangeRate();
 }
 
-var getExchangeRate = function() {
+var getExchangeRate = function () {
+  fetch(exchangeUrl).then(function (response) {
+    // check that code is viable
+    // store data from API into global object
+    if (response.ok) {
+      console.log(response);
+      myData = response.json();
+      myData.then(function (data) {
+        console.log("print full object of objects", data);
+        // stores the object 'rates' from the data response into
+        // global variable object
+        acceptedCodeRateObject = data.rates;
+        console.log("print rates", acceptedCodeRateObject);
+        i = 0;
 
-    fetch(exchangeUrl)
-        .then(function (response) {
-            // check that code is viable
-            // store data from API into global object
-            if (response.ok) {
-                // console.log(response);
-                myData = response.json();
-                myData.then(function (data) {
-                    // console.log("print full object of objects", data);
-                    // stores the object 'rates' from the data response into
-                    // global variable object
-                    acceptedCodeRateObject = data.rates;
-                    // console.log("print rates", acceptedCodeRateObject);
-                    i = 0
-
-                    // Dynamically create dropdown to select currency
-                    var myOptions = acceptedCodeRateObject;
-                    var mySelect = $('#my-currency');                    
-                    // for each key-value (myCountryCode-myExchangeRate) pair
-                    // append the country code as an option in the drop down
-                    // use the exchange rate to perform operation to convert data
-                    $.each(myOptions, function(myCountryCode, myExchangeRate) {
-                        mySelect.append($(`<option data-name="${i++}"></option>`).val(myCountryCode).html(myCountryCode));
-                        // Adds all currency rates to the global array
-                        acceptedCurrencyRateArray.push(myExchangeRate)               
-                    });
-                });
-              } else {
-                //TODO: Alert needs to be removed
-                alert('Error: ' + response.statusText);
-              }              
+        // Dynamically create dropdown to select currency
+        var myOptions = acceptedCodeRateObject;
+        var mySelect = $("#my-currency");
+        // for each key-value (myCountryCode-myExchangeRate) pair
+        // append the country code as an option in the drop down
+        // use the exchange rate to perform operation to convert data
+        $.each(myOptions, function (myCountryCode, myExchangeRate) {
+          mySelect.append(
+            $(`<option data-name="${i++}"></option>`)
+              .val(myCountryCode)
+              .html(myCountryCode)
+          );
+          acceptedCurrencyRateArray.push(myExchangeRate);
         });
-}
-
-var currencyExchange = function() {
-  // Checks if an optional currency is used to convert after USD is established
-  if (acceptedCurrencyCodeArray.includes(currencyFieldEl.value)) {    
-    // Checks for a specified currencies dataset value
-    let rateValue = currencyFieldEl.selectedOptions[0].dataset.name;
-    // TODO: Converted result needs to be moved to an output
-    // Uses currency dataset value to multiply USD value with currency exchange rates
-    console.log(newUsdValue*acceptedCurrencyRateArray[rateValue]);
+      });
+    } else {
+      //TODO: Alert needs to be removed
+      alert("Error: " + response.statusText);
     }
+  });
+};
+
+var currencyExchange = function () {
+  if (acceptedCurrencyCodeArray.includes(currencyFieldEl.value)) {
+    let rateValue = currencyFieldEl.selectedOptions[0].dataset.name;
+    console.log(newUsdValue * acceptedCurrencyRateArray[rateValue]);
+  }
 };
 
 // EVENT HANDLERS-----------------------------------
@@ -144,22 +143,18 @@ let formSubmitHandler = function (event) {
 
   // Trims API input, then converts and pushes it to an array for verifacation
   let api = apiFieldEl.value.trim();
-  let apiArray = []
-  apiArray.push(api.split(''));
-  
-  // Checks if an API Key that is entered is of a valid length
-  if (apiArray.length === 72) {
-    gwApiKey = api
-    // Runs the GW2 API function on the entered API key
-    getApi(gwApiKey)
+
+  $(".usd-card").removeClass("hide");
+  if (api) {
+    gwApiKey = api;
+    getApi(gwApiKey);
   } else {
     //TODO: Alert needs to be removed
-    alert('Please enter a valid 72 digit Guild Wars 2 API Key');
+    alert("Please enter a valid Guild Wars 2 API Key");
   }
 };
 
-enterEl.addEventListener('click', formSubmitHandler);
+enterEl.addEventListener("click", formSubmitHandler);
 
 // RUN PROGRAM--------------------------------------
 init();
-
